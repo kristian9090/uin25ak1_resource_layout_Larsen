8707050399f4e92c7e3e39203069019bc7df2deb
kristian <Kristian.larsen38@gmail.com>
2025-01-30 17:26:23 +0100

    nei men nå er alt rydda i

--- Arbeidskrav_1/ressurser.js
+++ Arbeidskrav_1/ressurser.js
@@ -108,14 +108,14 @@ function whiteButton(category) {
     });
 }
 
+// Deklarerer en funksjon som viser informasjon om forskjellige temaer innenfor webutvikling
+function showInfo(category) {
 
-// Deklarerer en funksjon som kjører når siden er ferdig lastet
-function displayLoad() {
-    // .filter og finner resource som er HTML, da den skal bli vist først
-    const htmlResources = resources.filter(resource => resource.category === "HTML")
-
-    // går igjennom category HTML ved hjelp av .map()
-    let resourcesHTML = htmlResources.map(resource => `
+    // bruker .find for å se om det finnes en resource med samme kategori som er valgt i knappen
+    const resource = resources.filter(resource => resource.category === category)
+    
+    // tom variabel som senere oppdateres
+    let resourcesHTML = resource.map(resource => `
         <article>
             <h3>${resource.category}</h3>
             <p>${resource.text}</p>
@@ -128,42 +128,11 @@ function displayLoad() {
     // skriver ut til id info-section
     document.getElementById("info-section").innerHTML = resourcesHTML
 
-    // Kjører funksjonen whiteButton med HTML som parameter
-    whiteButton("HTML")
-}
-
-// Legger til en event listener for å kjøre showLoad når siden er ferdig lastet
- window.onload = displayLoad
-
-
-// Deklarerer en funksjon som viser informasjon om forskjellige temaer innenfor webutvikling
-function showInfo(category) {
-
-    // bruker .find for å se om det finnes en resource med samme kategori som er valgt i knappen
-    const resource = resources.find(resource => resource.category === category);
-    
-    // tom variabel som senere oppdateres
-    let resourcesHTML = "";
-    if (resource) { 
-        resourcesHTML += `
-            <article>
-            <h3>${resource.category}</h3>
-            <p>${resource.text}</p>
-            <ul>
-        `;
-        // bruker .forEach for å gå gjennom kildene og legge til hver kilde som en listeelement
-        resource.sources.forEach(source => {
-            resourcesHTML += `<li><a href="${source.url}">${source.title}</a></li>`;
-        });
-        resourcesHTML += `
-            </ul>
-        </article>
-        `;
-    }
-
-    // Skriver ut artikkel kortet i HTML
-    document.getElementById("info-section").innerHTML = resourcesHTML;
    
     // Kjører funksjonen whiteButton med category som parameter
     whiteButton(category)
 }
+
+
+// Kjører showInfo med HTML i parameter slik at riktig kategori vises når siden er ferdig lastet
+window.onload = showInfo("HTML")
