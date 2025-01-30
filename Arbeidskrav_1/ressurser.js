b50d662cab3b22136fc37b0997d46cae9778688e
kristian <Kristian.larsen38@gmail.com>
2025-01-30 17:30:23 +0100

    ferdig kommentarer

--- Arbeidskrav_1/ressurser.js
+++ Arbeidskrav_1/ressurser.js
@@ -104,17 +104,17 @@ function whiteButton(category) {
     document.querySelectorAll('button').forEach(button => {
         // bruker .toggle for å legge til classen 'active' i knappen som er valgt
         // mellomlagrer button for å finne knappen, og .innerHTML for å manipulere innholdet i knappen og .includes for å se om kategorien er i knappen
-        button.classList.toggle('active', button.innerHTML.includes(category));
-    });
+        button.classList.toggle('active', button.innerHTML.includes(category))
+    })
 }
 
 // Deklarerer en funksjon som viser informasjon om forskjellige temaer innenfor webutvikling
 function showInfo(category) {
 
-    // bruker .find for å se om det finnes en resource med samme kategori som er valgt i knappen
+    // bruker .filter for å finne riktig kategori i resources som skal vises
     const resource = resources.filter(resource => resource.category === category)
     
-    // tom variabel som senere oppdateres
+    // maper gjennom resource og riktig kategori og da viser riktige verdier
     let resourcesHTML = resource.map(resource => `
         <article>
             <h3>${resource.category}</h3>
@@ -123,16 +123,14 @@ function showInfo(category) {
                 ${resource.sources.map(source => `<li><a href="${source.url}">${source.title}</a></li>`).join('')}
             </ul>
         </article>
-    `).join('');
+    `).join('')
 
     // skriver ut til id info-section
     document.getElementById("info-section").innerHTML = resourcesHTML
 
-   
     // Kjører funksjonen whiteButton med category som parameter
     whiteButton(category)
 }
-
 
 // Kjører showInfo med HTML i parameter slik at riktig kategori vises når siden er ferdig lastet
 window.onload = showInfo("HTML")
